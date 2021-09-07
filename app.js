a3fbfe0dfb71c63031d586967f4865b834ad5fa1
MadhawaJayagoda <nipunajayagoda@gmail.com>
2021-09-07 20:52:57 +0530

    Signup Interface added

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv/config');
 
+const {google} = require('googleapis');
+const OAuth2Data = require('./resources/credentials');
+
 //Middleware
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static("public"));
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
