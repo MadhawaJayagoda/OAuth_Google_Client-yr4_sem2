ecf83a577693fb6785cd2367276458588f5e021a
MadhawaJayagoda <nipunajayagoda@gmail.com>
2021-09-08 17:02:45 +0530

    Login with Google Authorization view added v4

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
+const multer = require('multer');
 require('dotenv/config');
 
+let constants = require('./resources/constants');
 const {google} = require('googleapis');
-const OAuth2Data = require('./resources/credentials');
 
 //Middleware
 app.use(bodyParser.json());
@@ -20,11 +22,23 @@ const postRoutes = require('./routes/posts');
 
 app.use('/posts', postRoutes);
 
+const OAuth2Client = new google.auth.OAuth2(
+  constants.CLIENT_ID,
+  constants.CLIENT_SECRET,
+  constants.REDIRECT_URI
+);
+
+var authorized = false;
+
+var name;
+var profilePic;
 
 // ROUTES
 app.get('/', (req, res) => {
-   res.render("index")
+	res.render('index');
+
 });
+
 
 // start listening to the server
 app.listen(3000, () => {
