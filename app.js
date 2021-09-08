b4fe8ab21788de06d3fb28469bfdaff7ebf11289
MadhawaJayagoda <nipunajayagoda@gmail.com>
2021-09-08 17:09:58 +0530

    User.Profile Google Authorization added

--- app.js
+++ app.js
@@ -9,18 +9,11 @@ require('dotenv/config');
 let constants = require('./resources/constants');
 const {google} = require('googleapis');
 
-//Middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("public"));
 
-// Set the view engine to ejs
 app.set('view engine', 'ejs');
-
-// Import Routes
-const postRoutes = require('./routes/posts');
-
-app.use('/posts', postRoutes);
 
 const OAuth2Client = new google.auth.OAuth2(
   constants.CLIENT_ID,
@@ -35,8 +28,42 @@ var profilePic;
 
 // ROUTES
 app.get('/', (req, res) => {
-	res.render('index');
+	 if (!authorized) {
+        let url = OAuth2Client.generateAuthUrl({
+            access_type: constants.ACCESS_TYPE,
+            scope: constants.SCOPES
+        });
 
+        console.log(url);
+
+        res.render("index", {url:url});
+    } else {
+
+        let oAuth2 = google.oauth2({
+            auth: OAuth2Client,
+            version: 'v2'
+        });
+
+        // User info
+        oAuth2.userinfo.get(function (err, response) {
+            if (err) {
+                console.log(err.message);
+                throw err;
+            }
+
+            console.log(response.data);
+
+            name = response.data.name;
+            profilePic = response.data.picture;
+
+            res.render("success", {
+                name: name,
+                pic: profilePic,
+                success:false
+            });
+        })
+
+    }
 });
 
 
